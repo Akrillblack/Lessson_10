a584f013037ac55d37937e1f457d3cefc628c15f
Donatas Vidutis <donatasvidutis@gmail.com>
2022-10-18 18:56:29 +0300

    LSN-10
    Project creation

---
+++ Lesson-10/Lesson-10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson-10
+//
+//  Created by Donatas on 18.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
